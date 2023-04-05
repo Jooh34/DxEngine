b5a3c7309f3e9877417cdbc479591e79286ff70d
Jooh <mnh513@naver.com>
2023-04-05 21:32:12 +0900

    Tutorial7 done

---
+++ Tutorial7 - 3D Model Rendering/DxEngine/DxDefine.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#pragma once
+
+// 라이브러리 링크
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+// DirectXTex.lib
+
+// include
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <directxmath.h>
+#include "DirectXTex.h"
+using namespace DirectX;
+

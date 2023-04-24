9b8c0d70355ed73a0b7ba3012d5431996845da99
Jooh <mnh513@naver.com>
2023-04-24 23:38:56 +0900

    tutorial 13, 15

---
+++ Tutorial15 - FPS CPU/DxEngine/inputclass.h
@@ -0,0 +1,62 @@
+#pragma once
+#ifndef _INPUTCLASS_H_
+#define _INPUTCLASS_H_
+//You need to define the version of Direct Input you are using in the header or the compiler will generate annoying messages that it is defaulting to version 8.
+
+///////////////////////////////
+// PRE-PROCESSING DIRECTIVES //
+///////////////////////////////
+#define DIRECTINPUT_VERSION 0x0800
+//The following two libraries need to be linked for Direct Input to work.
+
+/////////////
+// LINKING //
+/////////////
+#pragma comment(lib, "dinput8.lib")
+#pragma comment(lib, "dxguid.lib")
+//This is the required header for Direct Input.
+
+//////////////
+// INCLUDES //
+//////////////
+#include <dinput.h>
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: InputClass
+////////////////////////////////////////////////////////////////////////////////
+class InputClass
+{
+public:
+	InputClass();
+	InputClass(const InputClass&);
+	~InputClass();
+
+	bool Initialize(HINSTANCE, HWND, int, int);
+	void Shutdown();
+	bool Frame();
+
+	bool IsEscapePressed();
+	void GetMouseLocation(int&, int&);
+
+private:
+	bool ReadKeyboard();
+	bool ReadMouse();
+	void ProcessInput();
+
+private:
+	//The first three private member variables are the interfaces to Direct Input, the keyboard device, and the mouse device.
+
+	IDirectInput8* m_directInput;
+	IDirectInputDevice8* m_keyboard;
+	IDirectInputDevice8* m_mouse;
+	//The next two private member variables are used for recording the current state of the keyboardand mouse devices.
+
+	unsigned char m_keyboardState[256];
+	DIMOUSESTATE m_mouseState;
+
+	int m_screenWidth, m_screenHeight;
+	int m_mouseX, m_mouseY;
+};
+
+#endif
---
+++ Tutorial15 - FPS CPU/DxEngine/textclass.h
@@ -0,0 +1,61 @@
+#ifndef _TEXTCLASS_H_
+#define _TEXTCLASS_H_
+
+///////////////////////
+// MY CLASS INCLUDES //
+///////////////////////
+#include "fontclass.h"
+#include "fontshaderclass.h"
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: TextClass
+////////////////////////////////////////////////////////////////////////////////
+class TextClass
+{
+private:
+	//SentenceType is the structure that holds the rendering information for each text sentence.
+
+	struct SentenceType
+	{
+		ID3D11Buffer* vertexBuffer, * indexBuffer;
+		int vertexCount, indexCount, maxLength;
+		float red, green, blue;
+	};
+
+	struct VertexType
+	{
+		XMFLOAT3 position;
+		XMFLOAT2 texture;
+	};
+
+public:
+	TextClass();
+	TextClass(const TextClass&);
+	~TextClass();
+
+	bool Initialize(ID3D11Device*, ID3D11DeviceContext*, HWND, int, int, XMMATRIX);
+	void Shutdown();
+	bool Render(ID3D11DeviceContext*, XMMATRIX, XMMATRIX);
+
+	bool SetFps(int, ID3D11DeviceContext*);
+	bool SetCpu(int, ID3D11DeviceContext*);
+
+private:
+	bool InitializeSentence(SentenceType**, int, ID3D11Device*);
+	bool UpdateSentence(SentenceType*, char*, int, int, float, float, float, ID3D11DeviceContext*);
+	void ReleaseSentence(SentenceType**);
+	bool RenderSentence(ID3D11DeviceContext*, SentenceType*, XMMATRIX, XMMATRIX);
+
+private:
+	FontClass* m_Font;
+	FontShaderClass* m_FontShader;
+	int m_screenWidth, m_screenHeight;
+	XMMATRIX m_baseViewMatrix;
+	//We will use two sentences in this tutorial.
+
+	SentenceType* m_sentence1;
+	SentenceType* m_sentence2;
+};
+
+#endif
